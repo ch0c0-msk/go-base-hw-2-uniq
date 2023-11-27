8cb24276cee072c83cce7d27215c19a4e72c0b18
Tim <general.lee121@yandex.ru>
2023-11-27 19:33:51 +0300

    Add main file

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"go-base-hw-2-uniq/uniqParser"
+	"go-base-hw-2-uniq/utils"
+	"log"
+)
+
+func main() {
+	args, opt, err := utils.GetArgsAndOptions()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inputFile, err := utils.GetInputFile(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inputLines, err := utils.ReadLinesFromFile(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outputLines := uniqParser.Uniq(inputLines, opt)
+
+	outputFile, err := utils.GetOutputFile(args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = utils.WriteLinesIntoFile(outputFile, outputLines)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
