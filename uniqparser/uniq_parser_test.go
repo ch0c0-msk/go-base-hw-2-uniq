27403a8e06cd358b58347a86481a059c1637dce0
Tim <general.lee121@yandex.ru>
2023-12-24 20:55:01 +0300

    Add table-driven testing

--- uniqparser/uniq_parser_test.go
+++ uniqparser/uniq_parser_test.go
@@ -1,57 +1,61 @@
 package uniqparser
 
 import (
-	"strings"
+	"go-base-hw-2-uniq/cmdutils"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"go-base-hw-2-uniq/cmdutils"
 )
 
-var testLines = []string{"i love", "i love", "we love", "we love", "we ll love", "you love"}
+func TestFlags(t *testing.T) {
+	testCases := []struct {
+		name   string
+		input  []string
+		opt    *cmdutils.Options
+		output []string
+	}{
+		{
+			"NoneFlag",
+			[]string{"i love", "i love", "we love", "we love", "we ll love", "you love"},
+			&cmdutils.Options{},
+			[]string{"i love", "we love", "we ll love", "you love"},
+		},
+		{
+			"CFlag",
+			[]string{"i love", "i love", "we love", "we love", "we ll love", "you love"},
+			&cmdutils.Options{CFlag: true},
+			[]string{"2 i love", "2 we love", "1 we ll love", "1 you love"},
+		},
+		{
+			"DFlag",
+			[]string{"i love", "i love", "we love", "we love", "we ll love", "you love"},
+			&cmdutils.Options{DFlag: true},
+			[]string{"i love", "we love"},
+		},
+		{
+			"UFlag",
+			[]string{"i love", "i love", "we love", "we love", "we ll love", "you love"},
+			&cmdutils.Options{UFlag: true},
+			[]string{"we ll love", "you love"},
+		},
+		{
+			"IFlag",
+			[]string{"i love", "I LOVE", "we love", "we love", "we ll love", "you love"},
+			&cmdutils.Options{IFlag: true, DFlag: true},
+			[]string{"i love", "we love"},
+		},
+		{
+			"FFlagAndSFlag",
+			[]string{"i llove", "we wlove"},
+			&cmdutils.Options{FFlag: 1, SFlag: 1, DFlag: true},
+			[]string{"i llove"},
+		},
+	}
 
-func TestUniqParserWithoutFlags(t *testing.T) {
-	expected := []string{"i love", "we love", "we ll love", "you love"}
-	opt := &cmdutils.Options{}
-	actual := Uniq(testLines, opt)
-	assert.Equal(t, expected, actual)
-}
-
-func TestUniqParserWithCFlag(t *testing.T) {
-	expected := []string{"2 i love", "2 we love", "1 we ll love", "1 you love"}
-	opt := &cmdutils.Options{CFlag: true}
-	actual := Uniq(testLines, opt)
-	assert.Equal(t, expected, actual)
-}
-
-func TestUniqParserWithDFlag(t *testing.T) {
-	expected := []string{"i love", "we love"}
-	opt := &cmdutils.Options{DFlag: true}
-	actual := Uniq(testLines, opt)
-	assert.Equal(t, expected, actual)
-}
-
-func TestUniqParserWithUFlag(t *testing.T) {
-	expected := []string{"we ll love", "you love"}
-	opt := &cmdutils.Options{UFlag: true}
-	actual := Uniq(testLines, opt)
-	assert.Equal(t, expected, actual)
-}
-
-func TestUniqParserWithIFlag(t *testing.T) {
-	expected := []string{"i love", "we love"}
-	opt := &cmdutils.Options{DFlag: true, IFlag: true}
-	lines := testLines
-	lines[1] = strings.ToUpper(lines[1])
-	actual := Uniq(lines, opt)
-	assert.Equal(t, expected, actual)
-}
-
-func TestUniqParserWithFFlagAndSFlag(t *testing.T) {
-	expected := []string{"i llove"}
-	opt := &cmdutils.Options{DFlag: true, FFlag: 1, SFlag: 1}
-	lines := []string{"i llove", "we wlove"}
-	actual := Uniq(lines, opt)
-	assert.Equal(t, expected, actual)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := Uniq(tc.input, tc.opt)
+			assert.Equal(t, tc.output, actual)
+		})
+	}
 }
